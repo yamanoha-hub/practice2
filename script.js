8000741dab2f6fdbe9bff5f5e83fa4ed2a1d99f5
yamanoha-hab <r12m097@yahoo.co.jp>
2021-07-13 23:56:21 +0900

    jqueryとプラグイン導入 メニュー作成

---
+++ script.js
@@ -0,0 +1,12 @@
+$(function(){
+  $('.nav-btn').on('click', function() {
+    $(this).toggleClass('active');
+    $('.open-menu').toggleClass('open');
+    return false;
+  });
+
+  $('#page-top').click(function() {
+    
+
+  });
+});
