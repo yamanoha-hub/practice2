e86a131aea21c6b2a09e84bcc4e2e3ddf5411101
yamanoha-hab <r12m097@yahoo.co.jp>
2021-07-29 22:04:46 +0900

    gulp追加　PostCSS追加

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,24 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
-// var sass = require('gulp-sass')(require('sass'));
+// var sass = require('gulp-sass');
+var sass = require('gulp-sass')(require('sass'));
+var sassGlob = require('gulp-sass-glob');
+var sourcemaps = require('gulp-sourcemaps');
+var plumber = require('gulp-plumber');
+var notify = require('gulp-notify');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var mqpacker = require('css-mqpacker');
+
 
 gulp.task('sass', function() {
   return gulp.src('./sass/**/*.scss')
+    .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))  //エラー時に停止させずメッセージを出す。
+    .pipe(sourcemaps.init())
+    .pipe(sassGlob())
     .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(postcss([mqpacker()]))
+    .pipe(postcss([autoprefixer()]))  //ベンダープレフレックスを自動付与
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./css'));
 });
 
